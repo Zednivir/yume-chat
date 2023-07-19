a6e3c6dc3cb366d33fa7102094071272575b939a
Zednivir <131299447+Zednivir@users.noreply.github.com>
2023-07-19 10:57:04 +0100

    Update setupProxy.js

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // Define a variable for the API URL
-const API_URL = process.env.API_URL || 'https://continuity-organize-geography-eg.trycloudflare.com/api';
+const API_URL = process.env.API_URL || 'https://child-authorization-cities-say.trycloudflare.com/api';
 
 module.exports = function (app) {
   app.use(
