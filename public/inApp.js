ece1459d59595969ae11392b43068bbe9630b8f8
Zednivir <nry.yassin@gmail.com>
2023-08-10 20:59:59 +0100

    wahoo

--- public/inApp.js
+++ public/inApp.js
@@ -1,36 +1,36 @@
 addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggle-sidebar");
-  const closeUser = document.getElementById("closeUser");
-  const sidebar = document.querySelector(".sidebar");
-  const userButton = document.getElementById("userButton");
-  const userTab = document.querySelector(".userTab");
-  const messageContainer = document.querySelector(".message-container");
-  const sendButton = document.getElementById("sendButton");
-  const inputElement = document.getElementById("messageInput");
-  const settingsButton = document.getElementById("gearButton");
-  const settingsTab = document.querySelector(".settingsTab");
-  const closeSettings = document.getElementById("closeSettings");
-  const uploadButton = document.getElementById("uploadButton");
-  const avatarFileInput = document.getElementById("avatarFile");
-  const imgElement = document.querySelector(".loginTab img");
-  const userAvatarElement = document.querySelector(".userAvatar");
-  const bugButton = document.getElementById("bugButton");
-  const debugTab = document.querySelector(".debugTab");
-  const aiSettingsButton = document.getElementById('aiSettings');
-  const yumeSettingsButton = document.getElementById('yumeSettings');
-  const yumeTab = document.querySelector('.yumeTab');
-  const uiSettingsButton = document.getElementById('uiSettings');  
-  const uiTab = document.querySelector('.uiTab');
-  const fileButton = document.getElementById('fileButton');
-  const slider = document.getElementById("mySlider");
-  const sliderValue = document.getElementById("sliderValue");
+    const toggleButton = document.getElementById("toggle-sidebar");
+    const closeUser = document.getElementById("closeUser");
+    const sidebar = document.querySelector(".sidebar");
+    const userButton = document.getElementById("userButton");
+    const userTab = document.querySelector(".userTab");
+    const messageContainer = document.querySelector(".message-container");
+    const sendButton = document.getElementById("sendButton");
+    const inputElement = document.getElementById("messageInput");
+    const settingsButton = document.getElementById("gearButton");
+    const settingsTab = document.querySelector(".settingsTab");
+    const closeSettings = document.getElementById("closeSettings");
+    const uploadButton = document.getElementById("uploadButton");
+    const avatarFileInput = document.getElementById("avatarFile");
+    const imgElement = document.querySelector(".loginTab img");
+    const userAvatarElement = document.querySelector(".userAvatar");
+    const bugButton = document.getElementById("bugButton");
+    const debugTab = document.querySelector(".debugTab");
+    const aiSettingsButton = document.getElementById('aiSettings');
+    const yumeSettingsButton = document.getElementById('yumeSettings');
+    const yumeTab = document.querySelector('.yumeTab');
+    const uiSettingsButton = document.getElementById('uiSettings');  
+    const uiTab = document.querySelector('.uiTab');
+    const fileButton = document.getElementById('fileButton');
+    const slider = document.getElementById("mySlider");
+    const sliderValue = document.getElementById("sliderValue");
 
-  // Function to update the slider and text box value
+    // Function to update the slider and text box value
   function updateValue() {
     sliderValue.value = slider.value;
   }
 
-  // Event listener for slider input change
+    // Event listener for slider input change
   slider.addEventListener("input", updateValue);
 
   // Event listener for text box input change
@@ -84,85 +84,51 @@ addEventListener("DOMContentLoaded", function () {
     yumeTab.classList.remove('active');
     uiTab.classList.remove('active');
   });
-
-  // Declare userDisplayName and openAIKey variables
-  let userDisplayName = "User";
-  let openAIKey = null;
-
-  // Function to save the OpenAI key
-  function saveOpenAIKey() {
-    const openAIKeyInput = document.getElementById("openAIKey");
-    openAIKey = openAIKeyInput.value.trim();
-    settingsTab.classList.remove("active"); // Close the settings tab
-  }
-
-  // Function to send user's message to OpenAI and receive bot's reply
-  async function sendToOpenAI(messageText) {
-    const endpoint = "https://api.openai.com/v1/engines/davinci/completions";
-    const prompt = userDisplayName + ": " + messageText;
-    const maxTokens = 150;
-    const temperature = 1;
-
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + openAIKey,
-    };
-
-    const data = JSON.stringify({
-      prompt: prompt,
-      max_tokens: maxTokens,
-      temperature: temperature,
+  
+    // Function to toggle the settings tab
+    settingsButton.addEventListener('click', function() {
+        if (userTab.classList.contains("active")) {
+            userTab.classList.remove("active");
+          }
+          settingsTab.classList.toggle("active");
+    });
+  
+    // Function to close the settings tab
+    closeSettings.addEventListener('click', function() {
+      settingsTab.classList.toggle('active');
+    });
+  
+    // Function to toggle the settings tab with the "s" key
+    document.addEventListener('keydown', function(event) {
+      const inputElements = Array.from(document.getElementsByTagName('input'));
+      const textareaElements = Array.from(document.getElementsByTagName('textarea'));
+      const focusedElements = inputElements.concat(textareaElements).filter(element => element === document.activeElement);
+  
+      if (event.key === 's' && !focusedElements.length && document.activeElement !== inputElement) {
+        settingsButton.click();
+      }
+    });
+  
+    // Function to toggle the sidebar
+    toggleButton.addEventListener('click', function() {
+        sidebar.classList.toggle("active");
+        userTab.classList.toggle("push");
+        settingsTab.classList.toggle("push");
+        debugTab.classList.toggle("push");
+        adjustMessageContainer();
+    });
+  
+    document.addEventListener('keydown', function(event) {
+      const inputElements = Array.from(document.getElementsByTagName('input'));
+      const textareaElements = Array.from(document.getElementsByTagName('textarea'));
+      const focusedElements = inputElements.concat(textareaElements).filter(element => element === document.activeElement);
+  
+      if (event.key === 'm' && !focusedElements.length && document.activeElement !== inputElement) {
+        toggleButton.click();
+      }
     });
 
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: headers,
-      body: data,
-    });
-
-    const result = await response.json();
-    return result.choices[0].text.trim();
-  }
-
-  function adjustMessageContainer() {
-    const chatContainer = document.querySelector(".chat-container");
-    const inputContainer = document.querySelector(".input-container");
-    const sent = document.querySelector(".sent");
-
-    if (
-      debugTab.classList.contains("active") &&
-      sidebar.classList.contains("active")
-    ) {
-      // If both debug-tab and sidebar are active, do something different
-      messageContainer.style.width = "68.34%";
-      messageContainer.style.marginLeft = "410px";
-      inputContainer.style.marginLeft = "211px";
-    } else if (debugTab.classList.contains("active")) {
-      // If only debug-tab is active
-      messageContainer.style.width = "84.15%";
-      messageContainer.style.marginLeft = "205px";
-      inputContainer.style.marginLeft = "0px";
-    } else if (sidebar.classList.contains("active")) {
-      // If only sidebar is active
-      messageContainer.style.width = "84.15%";
-      messageContainer.style.marginLeft = "205px";
-      inputContainer.style.marginLeft = "211px";
-    } else {
-      // If neither is active
-      messageContainer.style.width = "100%";
-      messageContainer.style.marginLeft = "0";
-      inputContainer.style.marginLeft = "0";
-    }
-
-    const headerHeight = document.querySelector("header").offsetHeight;
-    const inputContainerHeight = inputContainer.offsetHeight;
-    const messageContainerHeight =
-      chatContainer.offsetHeight - headerHeight - inputContainerHeight;
-    messageContainer.style.height = `${messageContainerHeight}px`;
-    scrollToBottom();
-  }
-
-  // Function to toggle the debug tab
+    // Function to toggle the debug tab
   bugButton.addEventListener("click", function () {
     debugTab.classList.toggle("active");
     userTab.classList.toggle("double");
@@ -204,130 +170,46 @@ addEventListener("DOMContentLoaded", function () {
       reader.readAsDataURL(file);
     }
   }
-
-  // Get the username input element
-  const usernameInput = document.getElementById("Username");
-
-  // Function to update the user's display name
-  usernameInput.addEventListener("input", function () {
-    const newUsername = usernameInput.value.trim();
-    userDisplayName = newUsername; // Update the user's display name
-  });
-
-  uploadButton.addEventListener("click", function () {
-    avatarFileInput.click();
-  });
-
-  avatarFileInput.addEventListener("change", handleFileUpload);
-
-  // Add event listener to handle file selection and upload
-  avatarFileInput.addEventListener("change", handleFileUpload);
-
-  // Function to toggle the settings tab
-  settingsButton.addEventListener("click", function () {
-    if (userTab.classList.contains("active")) {
-      userTab.classList.remove("active");
-    }
-    settingsTab.classList.toggle("active");
-  });
-
-  // Function to close the settings tab
-  closeSettings.addEventListener("click", function () {
-    settingsTab.classList.toggle("active");
-  });
-
-  // Function to toggle the settings tab with the "s" key
-  document.addEventListener("keydown", function (event) {
-    const inputElements = Array.from(document.getElementsByTagName("input"));
-    const textareaElements = Array.from(
-      document.getElementsByTagName("textarea")
-    );
-    const focusedElements = inputElements
-      .concat(textareaElements)
-      .filter((element) => element === document.activeElement);
-
-    if (
-      event.key === "s" &&
-      !focusedElements.length &&
-      document.activeElement !== inputElement
-    ) {
-      settingsButton.click();
-    }
-  });
-
-  // Function to toggle the sidebar
-  toggleButton.addEventListener("click", function () {
-    sidebar.classList.toggle("active");
-    userTab.classList.toggle("push");
-    settingsTab.classList.toggle("push");
-    debugTab.classList.toggle("push");
-    adjustMessageContainer();
-  });
-
-  document.addEventListener("keydown", function (event) {
-    const inputElements = Array.from(document.getElementsByTagName("input"));
-    const textareaElements = Array.from(
-      document.getElementsByTagName("textarea")
-    );
-    const focusedElements = inputElements
-      .concat(textareaElements)
-      .filter((element) => element === document.activeElement);
-
-    if (
-      event.key === "m" &&
-      !focusedElements.length &&
-      document.activeElement !== inputElement
-    ) {
-      toggleButton.click();
-    }
-  });
-
-  // Function to toggle the user settings tab
-  userButton.addEventListener("click", function () {
-    if (settingsTab.classList.contains("active")) {
-      settingsTab.classList.remove("active");
-    }
-    userTab.classList.toggle("active");
-  });
-
-  // Function to close the user settings tab
-  closeUser.addEventListener("click", function () {
-    userTab.classList.toggle("active");
-  });
-
-  // Function to toggle the user settings tab with the "u" key
-  document.addEventListener("keydown", function (event) {
-    const inputElements = Array.from(document.getElementsByTagName("input"));
-    const textareaElements = Array.from(
-      document.getElementsByTagName("textarea")
-    );
-    const focusedElements = inputElements
-      .concat(textareaElements)
-      .filter((element) => element === document.activeElement);
-
-    if (
-      event.key === "u" &&
-      !focusedElements.length &&
-      document.activeElement !== inputElement
-    ) {
-      userButton.click();
-    }
-  });
-
-  sendButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    sendMessage();
-  });
-
-  inputElement.addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && !event.shiftKey) {
+  
+    // Function to toggle the user settings tab
+    userButton.addEventListener('click', function() {
+        if (settingsTab.classList.contains("active")) {
+            settingsTab.classList.remove("active");
+          }
+          userTab.classList.toggle("active");
+    });
+  
+    // Function to close the user settings tab
+    closeUser.addEventListener('click', function() {
+      userTab.classList.toggle('active');
+    });
+  
+    // Function to toggle the user settings tab with the "u" key
+    document.addEventListener('keydown', function(event) {
+      const inputElements = Array.from(document.getElementsByTagName('input'));
+      const textareaElements = Array.from(document.getElementsByTagName('textarea'));
+      const focusedElements = inputElements.concat(textareaElements).filter(element => element === document.activeElement);
+  
+      if (event.key === 'u' && !focusedElements.length && document.activeElement !== inputElement) {
+        userButton.click();
+      }
+    });
+  
+    sendButton.addEventListener('click', function(event) {
       event.preventDefault();
       sendMessage();
-    }
-  });
+    });
 
-
-  // Function to handle bot's replies
+    inputElement.addEventListener("keydown", function (event) {
+        if (event.key === "Enter" && !event.shiftKey) {
+          event.preventDefault();
+          sendMessage();
+        }
+      });
+    
+    let userDisplayName = "User";
+  
+    // Function to handle bot's replies
   async function handleBotReply(messageText) {
     // Add a rate limiting mechanism to avoid making requests too frequently
     if (handleBotReply.isSendingRequest) {
@@ -486,23 +368,56 @@ addEventListener("DOMContentLoaded", function () {
     return messageElement;
   }
 
-  function scrollToBottom() {
-    messageContainer.scrollTop = messageContainer.scrollHeight;
-  }
-
-  function adjustInputHeight() {
-    inputElement.style.height = "auto";
-    inputElement.style.height = `${inputElement.scrollHeight}px`;
+    function scrollToBottom() {
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+  
+    function adjustInputHeight() {
+      inputElement.style.height = 'auto';
+      inputElement.style.height = `${inputElement.scrollHeight}px`;
+      adjustMessageContainer();
+    }
+  
+    function adjustMessageContainer() {
+        const chatContainer = document.querySelector(".chat-container");
+        const inputContainer = document.querySelector(".input-container");
+            
+        if (
+          debugTab.classList.contains("active") &&
+          sidebar.classList.contains("active")
+        ) {
+          // If both debug-tab and sidebar are active, do something different
+          messageContainer.style.width = "68.34%";
+          messageContainer.style.marginLeft = "410px";
+          inputContainer.style.marginLeft = "211px";
+        } else if (debugTab.classList.contains("active")) {
+          // If only debug-tab is active
+          messageContainer.style.width = "84.15%";
+          messageContainer.style.marginLeft = "205px";
+          inputContainer.style.marginLeft = "0px";
+        } else if (sidebar.classList.contains("active")) {
+          // If only sidebar is active
+          messageContainer.style.width = "84.15%";
+          messageContainer.style.marginLeft = "205px";
+          inputContainer.style.marginLeft = "211px";
+        } else {
+          // If neither is active
+          messageContainer.style.width = "100%";
+          messageContainer.style.marginLeft = "0";
+          inputContainer.style.marginLeft = "0";
+        }
+    
+        const headerHeight = document.querySelector("header").offsetHeight;
+        const inputContainerHeight = inputContainer.offsetHeight;
+        const messageContainerHeight =
+          chatContainer.offsetHeight - headerHeight - inputContainerHeight;
+        messageContainer.style.height = `${messageContainerHeight}px`;
+        scrollToBottom();
+      }
+  
+    inputElement.addEventListener('input', adjustInputHeight);
+  
+    // Adjust message container height on initial page load
     adjustMessageContainer();
-  }
-
-  inputElement.addEventListener("input", adjustInputHeight);
-
-  // Adjust message container height on initial page load
-  adjustMessageContainer();
-
-  // Event listener for saving the OpenAI key
-  document
-    .getElementById("saveOpenAIKey")
-    .addEventListener("click", saveOpenAIKey);
-});
+  
+  });
